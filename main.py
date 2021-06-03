bfde1cf6c5e8a89732b66b200fba160d12e53dc6
Phenix-G <eiphenixg@gmail.com>
2021-06-04 00:19:58 +0800

    docs(main): add header info

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+##!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# @Author : Phenix-G
+# @File   : main.py
+# @Time   : 2021/06/04 00:19:05
 import os
 from pathlib import Path
 from typing import List
