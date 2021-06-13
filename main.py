25d4f3db108307e8afd8af7221d2ecdd9b5d8fbe
Phenix-G <eiphenixg@gmail.com>
2021-06-14 03:35:27 +0800

    feat(main): update

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ import os
 from pathlib import Path
 from typing import List
 
-import uvicorn
 from fastapi import FastAPI, UploadFile, File, Request
 from fastapi.middleware.cors import CORSMiddleware  # 跨域调用
 from fastapi.responses import RedirectResponse, FileResponse
@@ -24,7 +23,7 @@ app = FastAPI()
 #     allow_headers=['*'],
 # )
 templates = Jinja2Templates(directory='templates')
-app.mount('/templates', StaticFiles(directory='templates'), name='templates')
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 BASE_DIR = Path(__file__).resolve().parent
 VIRUS_EXTENSION_NAME = [
@@ -34,6 +33,7 @@ VIRUS_EXTENSION_NAME = [
 VIRUS = [virus for virus in map(lambda x: '.' + x, VIRUS_EXTENSION_NAME)]
 
 FILE_PATH = BASE_DIR.joinpath('file')
+TemplateResponse = templates.TemplateResponse
 
 
 def get_files():
@@ -44,7 +44,6 @@ def get_files():
 def write_file(file):
     with open('./file/{}'.format(file.filename), 'wb')as f:
         for i in iter(lambda: file.file.read(1024 * 1024 * 10), b''):
-            print(type(i))
             f.write(i)
 
 
@@ -54,25 +53,26 @@ async def upload(request: Request, files: List[UploadFile] = File(...)):
         if file.filename:
             extension_name = Path(file.filename).suffix
             if extension_name in VIRUS:
-                return templates.TemplateResponse('index.html',
-                                                  {'request': request, 'files': get_files(), 'fail': '上传失败'})
+                return TemplateResponse('index.html', {'request': request, 'files': get_files(), 'fail': '上传失败'})
             else:
                 write_file(file)
         else:
             return RedirectResponse('http://127.0.0.1:8000/')
-    return templates.TemplateResponse('index.html', {'request': request, 'files': get_files(), 'success': '上传成功'})
+    return TemplateResponse('index.html', {'request': request, 'files': get_files(), 'success': '上传成功'})
 
 
 @app.get("/download/{file_name}")
-def download(file_name: str):
+async def download(file_name: str) -> FileResponse:
     path = str(FILE_PATH.joinpath(file_name))
     return FileResponse(path)
 
 
 @app.get("/")
-async def index(request: Request):
-    return templates.TemplateResponse('index.html', {'request': request, 'files': get_files()})
+async def index(request: Request) -> TemplateResponse:
+    return TemplateResponse('index.html', {'request': request, 'files': get_files()})
 
 
 if __name__ == '__main__':
-    uvicorn.run(app, host='0.0.0.0', port=8000, )
+    import uvicorn
+
+    uvicorn.run('main:app', port=8000, debug=True, reload=True)
